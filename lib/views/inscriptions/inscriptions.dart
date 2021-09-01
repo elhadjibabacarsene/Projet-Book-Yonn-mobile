56beadd208be8560b0b6863042ab03a635fa7e5d
elhadjibabacarseneUVS <elhb.sene@uvs.edu.sn>
2021-09-01 18:42:15 +0000

    feat:homeInscription

--- lib/views/inscriptions/inscriptions.dart
+++ lib/views/inscriptions/inscriptions.dart
@@ -20,33 +20,82 @@ class _InscriptionState extends State<Inscription> {
         preferredSize: Size.fromHeight(70.0),
         child: AppBarInscription(),
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 58.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                child: Text(
-                  'Vous inscrire avec',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'SF Pro Display Regular',
-                    color: HexColor('#454f63'),
-                  ),
-                  ),
-              ),
-              SizedBox(height: 34,),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ButtonSocialInscription(text: 'assets/icones/google.png',),
-                    ButtonSocialInscription(text: 'assets/icones/facebook.png',),
-                    ButtonSocialInscription(text: 'assets/icones/apple.png',),
-                  ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 58.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Vous inscrire avec',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontFamily: 'SF Pro Display Regular',
+                      color: HexColor('#454f63'),
+                    ),
+                    ),
                 ),
-              )
-            ]
+                SizedBox(height: 40,),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ButtonSocialInscription(text: 'assets/icones/google.png',),
+                      ButtonSocialInscription(text: 'assets/icones/facebook.png',),
+                      ButtonSocialInscription(text: 'assets/icones/apple.png',),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 70),
+                Container(
+                  child: Text(
+                            'Ou',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: 'SF Pro Display Regular',
+                            color: HexColor('#454f63'),
+                            ),
+                      ),
+                ),
+                SizedBox(height: 70,),
+                Container(
+                  width: 305,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: HexColor('#2884ff'),
+                    ),
+                    child: Text(
+                        'S\'inscrire avec un email',
+                        style: TextStyle(
+                          color: HexColor('#ffffff'),
+                          fontFamily: 'Circular Std Bold',
+                          fontSize: 18,
+                          letterSpacing: 0.24,
+                        )
+                      ),
+                    onPressed: null,
+                  ),
+                ),
+                SizedBox(height: 270,),
+                Container(
+                  child: TextButton(
+                    onPressed: null,
+                    child: Text(
+                        'J\'ai déjà un compte',
+                        style: TextStyle(
+                          fontFamily: 'Circular Std Bold',
+                          fontSize: 19,
+                          letterSpacing: 0.24,
+                          color: HexColor('#2884ff'),
+                        )
+                      )
+                  ),
+                ),
+              ]
+            ),
           ),
         ),
     )
